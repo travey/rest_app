f9d6e7bf392aebfda3e9e3d0f0594289c81ac240
Travis Dailey <travismdailey@gmail.com>
2014-08-23 11:05:57 -0500

    controller formatted

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+  
   def about
   end
+  
   def restaurants
   end
+  
   def app
   end
+  
   def entree
   end
+  
   def checkout
   end
+  
   def complete
   end
 end
